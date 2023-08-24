6a91d9a09015633eae267eb77204abd80a68c069
asstronom <shliakhov.danylo@gmail.com>
2023-08-24 17:14:55 +0300

    feat: add TestIncrementalFizzBuzzIterator

--- main_test.go
+++ main_test.go
@@ -23,3 +23,23 @@ func TestFizzBuzz(t *testing.T) {
 		}
 	}
 }
+
+func TestIncrementalFizzBuzzIterator(t *testing.T) {
+	input := 20
+	output := []string{
+		"1", "2", "Fizz", "4", "Buzz",
+		"Fizz", "7", "8", "Fizz", "Buzz",
+		"11", "Fizz", "13", "14", "FizzBuzz",
+		"16", "17", "Fizz", "19", "Buzz",
+	}
+
+	fb := NewFizzBuzzModular(fzbzClassic, fzClassic, bzClassic)
+	fbIterator := NewIncrementalFizzBuzzIterator(fb)
+
+	assert := fbIterator.Iterate(input)
+	for i, v := range output {
+		if assert[i] != v {
+			t.Errorf("error on output #%d, expected %s, got %s", i, v, assert[i])
+		}
+	}
+}
