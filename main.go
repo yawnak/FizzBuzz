00273288d8583d3a5e188d3ab274ec35bd00c254
asstronom <shliakhov.danylo@gmail.com>
2023-08-24 16:15:22 +0300

    feat: add constructable fizzbuzz struct

--- main.go
+++ main.go
@@ -37,6 +37,28 @@ func (FizzBuzzClassic) Fizz(n int) string {
 	return fmt.Sprint(answerList)
 }
 
+type fizzbuzz struct {
+	fzbz func(int) bool
+	fz   func(int) bool
+	bz   func(int) bool
+}
+
+func (fb *fizzbuzz) FizzBuzz(n int) string {
+	answerList := make([]string, 0, n)
+	for i := 1; i <= n; i++ {
+		if fb.fzbz(i) {
+			answerList = append(answerList, FizzBuzz)
+		} else if fb.fz(i) {
+			answerList = append(answerList, Fizz)
+		} else if fb.bz(i) {
+			answerList = append(answerList, Buzz)
+		} else {
+			answerList = append(answerList, strconv.Itoa(i))
+		}
+	}
+	return fmt.Sprint(answerList)
+}
+
 func main() {
 	fb := FizzBuzzClassic{}
 	fmt.Println(fb.Fizz(20))
