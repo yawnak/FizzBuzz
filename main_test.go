9dbed9426c016fa8e7e283ec88188e433c9d4f83
asstronom <shliakhov.danylo@gmail.com>
2023-08-24 18:04:03 +0300

    feat: tests using testdata with 100 sample points

--- main_test.go
+++ main_test.go
@@ -43,3 +43,89 @@ func TestIncrementalFizzBuzzIterator(t *testing.T) {
 		}
 	}
 }
+
+// Test that uses 100 test points
+func TestFizzBuzzModularClassicWithData(t *testing.T) {
+	// Open the file
+	file, err := os.Open("test_data/test1.txt")
+	if err != nil {
+		fmt.Println("Error opening the file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		firstLine := scanner.Text()
+		_, err = strconv.Atoi(firstLine)
+		if err != nil {
+			fmt.Println("error converting to integer:", err)
+			return
+		}
+	} else {
+		fmt.Println("error reading first line:", scanner.Err())
+		return
+	}
+
+	// Iterate over each line and compare it with FizzBuzzer output
+	i := 1
+	fb := NewFizzBuzzModular(fzbzClassic, fzClassic, bzClassic) //create classic fizzbuzzer
+	for scanner.Scan() {
+		output := scanner.Text()
+		assert := fb.FizzBuzz(i)
+		//fmt.Println(output, assert)
+		if output != assert {
+			t.Errorf("wrong value, expected %s, got %s", output, assert)
+		}
+		i++
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println("error reading lines:", scanner.Err())
+	}
+}
+
+func TestIncrementalFizzBuzzIteratorWithData(t *testing.T) {
+	// Open the file
+	file, err := os.Open("test_data/test1.txt")
+	if err != nil {
+		fmt.Println("Error opening the file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	var n int
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		firstLine := scanner.Text()
+		n, err = strconv.Atoi(firstLine)
+		if err != nil {
+			fmt.Println("error converting to integer:", err)
+			return
+		}
+	} else {
+		fmt.Println("error reading first line:", scanner.Err())
+		return
+	}
+	//create iterator
+	fb := NewFizzBuzzModular(fzbzClassic, fzClassic, bzClassic) //classic fizzbuzzer
+	fbIterator := NewIncrementalFizzBuzzIterator(fb)
+	assert := fbIterator.Iterate(n) //do iterations
+
+	// Iterate over each line and print
+	i := 0
+	for scanner.Scan() {
+		output := scanner.Text()
+		//fmt.Println(output, assert[i])
+		if output != assert[i] {
+			t.Errorf("wrong value, expected %s, got %s", output, assert)
+		}
+		i++
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println("error reading lines:", scanner.Err())
+	}
+}
