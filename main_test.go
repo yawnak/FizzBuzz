6623945f293fec8a9d439de382b85ebca9b56e2a
asstronom <shliakhov.danylo@gmail.com>
2023-08-24 16:54:26 +0300

    feat: add simple test

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestSimpleTestFizzBuzz(t *testing.T) {
+	type Case struct {
+		input  int
+		output string
+	}
+	cases := []Case{
+		{1, "1"},
+		{3, Fizz},
+		{5, Buzz},
+		{15, FizzBuzz},
+	}
+
+	fb := NewFizzBuzz(fzbzClassic, fzClassic, bzClassic)
+
+	for _, c := range cases {
+		assert := fb.FizzBuzz(c.input)
+		if assert != c.output {
+			t.Errorf("wrong output fizzbuzz expected %s, got %s", c.output, assert)
+		}
+	}
+}
