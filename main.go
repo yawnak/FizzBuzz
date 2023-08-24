e7172e933d05d73504acbd683eec3a69578dff94
asstronom <shliakhov.danylo@gmail.com>
2023-08-24 15:56:51 +0300

    feat: add FizzBuzzClassic

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 const (
 	Fizz     = "Fizz"
 	Buzz     = "Buzz"
@@ -10,4 +15,24 @@ type FizzBuzzer interface {
 	Fizz(n int) string
 }
 
+type FizzBuzzClassic struct {
+}
 
+func (FizzBuzzClassic) Fizz(n int) string {
+	answerList := make([]string, 0, n)
+	for i := 1; i <= n; i++ {
+		var answer string
+		if i%15 == 0 {
+			answer = FizzBuzz
+		} else if i%3 == 0 {
+			answer = Fizz
+		} else if i%5 == 0 {
+			answer = Buzz
+		} else {
+			answer = strconv.Itoa(i)
+		}
+		answerList = append(answerList, answer)
+		answer = ""
+	}
+	return fmt.Sprint(answerList)
+}
